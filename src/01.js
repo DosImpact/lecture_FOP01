ee893044151ef72aca86f41d678679f1ce938fa3
dosimpact <ypd03008@gmail.com>
2021-05-05 16:32:33 +0900

    01

--- src/01.js
+++ src/01.js
@@ -1,7 +1,44 @@
 // 순수 함수
-
 // 일급 함수
-
 // 클로저
 
 // eg) add maker
+
+// 클로저를 사용해서, 인자값 하나가 결정되어 리턴되는 덧셈함수
+// ? bind 로도 가능 하다.!
+
+function add_maker(a) {
+  return function (b) {
+    return a + b;
+  };
+}
+
+var onePlus = add_maker(1);
+var twoPlus = add_maker(2);
+console.log(onePlus(100));
+console.log(twoPlus(100));
+
+var tenPlusBind = add_maker.bind(null, 10)();
+console.log(tenPlusBind(100));
+
+// eg) a op b  계산기
+
+// f1,f2 평가 후 , 리턴 인자를 각각
+//  f3 평가에 사용 후 결과 리턴
+function A_OP_B(f1, f2, f3) {
+  return f3(f1(), f2());
+}
+
+console.log(
+  A_OP_B(
+    () => {
+      return 20;
+    },
+    () => {
+      return 100;
+    },
+    (a, b) => {
+      return a * b;
+    }
+  )
+);
---
+++ src/02.js
@@ -0,0 +1,17 @@
+// map
+
+// filter
+
+var users = [
+  { id: 1, name: "DO", age: 23 },
+  { id: 2, name: "EG", age: 21 },
+  { id: 3, name: "GEO", age: 53 },
+  { id: 4, name: "QFO", age: 13 },
+  { id: 5, name: "QO", age: 28 },
+];
+
+console.log(users);
+
+// for 문 -> FOP 추상화 가능
+
+// eg) 절차 지향 filter & map
